5d120574a04ddb6b47d02426719cc8769eb3a855
Paul Heasley <paul@eql.xyz>
2022-07-27 11:37:20 +1000

    Add client-oauth2 client

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,39 @@
 import { Login } from "../components/Login"
+import { ClientOAuth2 } from "client-oauth2"
+
+const clientAuth = new ClientOAuth2({
+  clientId: process.env.REACT_APP_OAUTH2_CLIENT_ID,
+  clientSecret: process.env.REACT_APP_OAUTH2_CLIENT_SECRET,
+  accessTokenUri:
+    "https://eql-dev.us.authz.cloudentity.io/eql-dev/demo/oauth2/token",
+  authorizationUri:
+    "https://eql-dev.us.authz.cloudentity.io/eql-dev/demo/oauth2/authorize",
+  //   redirectUri: "http://example.com/auth/github/callback",
+  scopes: [
+    "address",
+    "dcr_register",
+    "email",
+    "introspect_tokens",
+    "list_clients_with_access",
+    "manage_consents",
+    "offline_access",
+    "openid",
+    "phone",
+    "profile",
+    "revoke_client_access",
+    "revoke_tokens",
+    "view_consents",
+  ],
+})
 
 export const LoginPage = () => {
   const handleSubmit = async (values, formikBag) => {
     console.log(values, formikBag)
+    const user = await clientAuth.owner.getToken(
+      values.email.toLowerCase(),
+      values.password
+    )
+    console.log(user)
   }
 
   return <Login onSubmit={handleSubmit} />
